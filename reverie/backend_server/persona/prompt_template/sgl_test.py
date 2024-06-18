d4509a92d6b3e44d75f7be061148b05e3204ad1c
Hao Kang <60107867+HaoKang-Timmy@users.noreply.github.com>
2024-06-18 00:06:23 -0700

    Update sgl_test.py

--- reverie/backend_server/persona/prompt_template/sgl_test.py
+++ reverie/backend_server/persona/prompt_template/sgl_test.py
@@ -1,33 +1,7 @@
 from sglang import function, system, user, assistant, gen, set_default_backend, RuntimeEndpoint
 import sglang as sgl
 from gpt_structure import llm_safe_generation
-# @staticmethod
-# def llm_call(prompt, max_tokens):
-#     set_default_backend(RuntimeEndpoint("http://localhost:30000"))
-#     @sgl.function
-#     def func_wrapper(s, prompt,max_tokens = max_tokens):
-#         # todo, support for priority scheduling for sgl
-#         s += prompt
-#         s += sgl.gen("response",
-#                         max_tokens=max_tokens)
 
-#     response = func_wrapper.run(prompt)
-#     return response["response"]
-
-# @staticmethod
-# def llm_call1(prompt):
-#     set_default_backend(RuntimeEndpoint("http://localhost:30000"))
-#     @sgl.function
-#     def func_wrapper(s, prompt):
-#         # todo, support for priority scheduling for sgl
-#         s += prompt
-#         s += sgl.gen("response",
-#                         )
-
-#     response = func_wrapper.run(prompt)
-#     return response["response"]
-# result = llm_call1("I want you to write a schedule for me, starting from 8:00 am to 5 p.m.")
-# print(result)
 
 def LLM_request(prompt,model_parameter):
   set_default_backend(RuntimeEndpoint("http://localhost:30000"))
@@ -53,32 +27,46 @@ def LLM_request(prompt,model_parameter):
     print('Failed: '+ str(e))
     print ("TOKEN LIMIT EXCEEDED")
     return "TOKEN LIMIT EXCEEDED"
+def create_prompt_input(persona, event_description, test_input=None): 
+    prompt_input = [persona.scratch.name,
+                    persona.scratch.get_str_iss(),
+                    persona.scratch.name,
+                    event_description]
+    return prompt_input
+  
+def __func_clean_up(gpt_response, prompt=""):
+    return gpt_response.split('"')[0].strip()
+
+def __func_validate(gpt_response, prompt=""): 
+  try: 
+    __func_clean_up(gpt_response, prompt)
+    return True
+  except:
+    return False 
+
+def get_fail_safe(): 
+  return "..."
+
+
+# ChatGPT Plugin ===========================================================
 def __chat_func_clean_up(gpt_response, prompt=""): ############
-  cr = gpt_response.strip()
-  if len(cr) > 3:
-    cr = cr[:3]
-  return cr
+  return gpt_response.split('"')[0].strip()
 
 def __chat_func_validate(gpt_response, prompt=""): ############
   try: 
-    __func_clean_up(gpt_response, prompt="")
-    if len(gpt_response) == 0: 
-      return False
-  except: return False
-  return True 
-def __func_clean_up(gpt_response, prompt=""):
-  cr = gpt_response.strip()
-  if len(cr) > 3:
-    cr = cr[:3]
-  return cr
-prompt = "Task: We want to understand the state of an object that is being used by someone. \n\nLet's think step by step. \nWe want to know about closet's state. \nStep 1. Yuriko Yamamoto is at/using the sleeping.\nStep 2. Describe the closet's state."
-example_output = "{output\:closet is idle}"
+    __func_clean_up(gpt_response, prompt)
+    return True
+  except:
+    return False 
+prompt = "[Statements]\nJennifer Moore is sleeping\nThis is Sam Moore's plan for Monday February 13: wake up and complete the morning routine at 5:00 am, .\nSam Moore is sleeping\nwaking up\nwaking up\n\n\nBased on the statements above, summarize Sam Moore and Jennifer Moore's relationship. What do they feel or know about each other?\n\n\n\""
+# example_output = "{\"output\"\:5}"
 # special_instruction = "only return the emoji without anyother things"
 # special_instruction = special_instruction = "The output should ONLY contain the phrase that should go in <fill in>."
-special_instruction = "output the answer in json format like {output: <result>}, give the answer to the problem, do not generate code or any unrelated results."
-fail_safe = "closet is idle"
-
+special_instruction = " Generate one set of results in json format like {\"output\": <output>} in about 20 words. Do not generate code."
+fail_safe = 5
+act_game_object = "sth"
+example_output = f"{{\"output\": ...}}"
 result = llm_safe_generation(prompt,example_output,special_instruction,3,fail_safe,
                              __chat_func_validate,__chat_func_clean_up,
-                             {"max_tokens":50,"temperature":0.3})
+                             {"max_tokens":100,"temperature":0.3,"frequency_penalty": 0.1})
 print("result",result)
